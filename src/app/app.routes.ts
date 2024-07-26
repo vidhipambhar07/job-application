0f58690504638c0c30a79f318d8d80e5da71803f
vidhi.pambhar <vidhi.pambhar@agileinfoways.com>
2024-07-26 15:45:24 +0530

    initial project setup with components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LayoutComponent,
+        pathMatch: "full"
+    },
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "application-form",
+        // component:
+    }
+];
---
+++ src/app/components/application-form/application-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './application-form.component.html',
+  styleUrl: './application-form.component.css'
+})
+export class ApplicationFormComponent {
+
+}
