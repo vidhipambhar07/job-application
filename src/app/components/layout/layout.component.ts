a1b6e4f59df76a95b0d11d8fd60cefeba604de2d
vidhi.pambhar <vidhi.pambhar@agileinfoways.com>
2024-07-29 14:05:14 +0530

    change login form design and layout design

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-
+  router = inject(Router)
+  onLogin() {
+    this.router.navigateByUrl('/admin-login');
+  }
+  onSubmit() {
+    this.router.navigateByUrl('/application-form')
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
+  router = inject(Router)
+
+  loginObject: any = {
+    username: '',
+    password: ''
+  };
+
+  private username = 'agileinfoway';
+  private password = 'agile123!';
+  private token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcl90eXBlIjotMSwiZW1haWwiOiJhZG1pbkBkYmQuY29tIiwiZGV2aWNlX2lkIjoic3RyaW5nIiwicGFzc3dvcmQiOiJuQWYwbjJpd2lpUHpSUmJQTEZlUjRnPT0iLCJwZXJtaXNzaW9ucyI6eyJhbGxvd19kZWxldGUiOjEsImFsbG93X2RlYWN0aXZlIjoxfSwiaWF0IjoxNzIxOTAwNzcxLCJleHAiOjE3MjI1MDU1NzF9.bwCmGjxffGhQv46llKdB8zVaHZOcD7VwtaWLq1AV-VQ';
+  private role = 'admin'
+  login() {
+    if (this.loginObject.username === this.username && this.loginObject.password === this.password) {
+      this.storeToken(this.token, this.role);
+      this.router.navigateByUrl('view-form');
+    } else {
+      alert('Invalid credentials');
+    }
+  }
+  private storeToken(token: string, role: string) {
+    localStorage.setItem('authToken', token);
+    localStorage.setItem('role', role);
+  }
 
 }
